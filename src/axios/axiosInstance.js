64905b5b454ba789ff76401b2b8e0020bc58b3cb
sanjay <sanjaysaravanakumar31@gmail.com>
2025-11-28 15:46:46 +0530

    pixel code

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5500",
+  // baseURL: "http://localhost:5500",
+  baseURL: "https://api.purfull.com",
   withCredentials: true, 
 });
 
